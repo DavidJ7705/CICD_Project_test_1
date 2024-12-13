473ded3537a18645e742aee321c59e70d6d4e4b6
Yi Zheng Tang <g00425075@atu.ie>
2024-12-13 15:06:02 +0000

    Created entity package with the person information

---
+++ src/main/java/ie/atu/project/Entity/Person.java
@@ -0,0 +1,33 @@
+package ie.atu.project.Entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Person {
+    private
+    String userId;
+
+    private
+    String name;
+
+    private
+    String userName;
+
+    private
+    String email;
+
+    private
+    String password;
+    //1 for user, 2 for mod
+    private int userType;
+    //FK
+    private int postId;
+
+    private int courseId;
+
+    private int commentId;
+}
