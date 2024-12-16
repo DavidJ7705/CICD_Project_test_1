ad16deadc5f90fa9a5a75664b63a2d5c8a5c4bc8
Yi Zheng Tang <g00425075@atu.ie>
2024-12-16 17:27:26 +0000

    Added another module for post page for specific module.

---
+++ PostPage/src/main/java/ie/atu/postpage/PostPageService.java
@@ -0,0 +1,4 @@
+package ie.atu.postpage;
+
+public class PostPageService {
+}
---
+++ PostPage/src/test/java/ie/atu/postpage/PostPageApplicationTests.java
@@ -0,0 +1,13 @@
+package ie.atu.postpage;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PostPageApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
