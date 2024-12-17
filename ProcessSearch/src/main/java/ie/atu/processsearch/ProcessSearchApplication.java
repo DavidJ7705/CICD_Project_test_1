f5a6b0aca1812078a33f8efad510bf8bc4e4381c
Yi Zheng Tang <g00425075@atu.ie>
2024-12-17 13:59:13 +0000

    Added proper server ports for each microservice. Also added search microservice module

---
+++ ProcessSearch/src/main/java/ie/atu/processsearch/ProcessSearchApplication.java
@@ -0,0 +1,13 @@
+package ie.atu.processsearch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProcessSearchApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProcessSearchApplication.class, args);
+    }
+
+}
---
+++ ProcessSearch/src/test/java/ie/atu/processsearch/ProcessSearchApplicationTests.java
@@ -0,0 +1,13 @@
+package ie.atu.processsearch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProcessSearchApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
