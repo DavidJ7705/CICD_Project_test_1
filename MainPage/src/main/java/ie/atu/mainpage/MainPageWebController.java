e9ef090d1521e42171ffe53374936dfc6d9bdc3f
G00419108@atu.ie <G00419108@atu.ie>
2024-12-19 17:45:46 +0000

    Added code to load main course page for the new endpoint /main/viewall which redirects getting all courses.

---
+++ MainPage/src/main/java/ie/atu/mainpage/MainPageWebController.java
@@ -0,0 +1,13 @@
+package ie.atu.mainpage;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class MainPageWebController {
+    @GetMapping("/main/viewall")
+    public String showCoursesPage() {
+        // Returns the HTML view (index.html) from templates
+        return "index";
+    }
+}
