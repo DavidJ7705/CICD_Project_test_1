9b4638deda94e66b61ea5230440ea9fc41039af7
TamerZraiq <118359972+TamerZraiq@users.noreply.github.com>
2024-12-22 17:53:11 +0000

    module database v2

--- ModulePage/src/main/resources/module_db.sql
+++ ModulePage/src/main/resources/module_db.sql
@@ -1,10 +1,8 @@
-CREATE DATABASE  IF NOT EXISTS `module_db` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci */ /*!80016 DEFAULT ENCRYPTION='N' */;
-USE `module_db`;
--- MySQL dump 10.13  Distrib 8.0.29, for Win64 (x86_64)
+-- MySQL dump 10.13  Distrib 8.0.36, for Win64 (x86_64)
 --
 -- Host: localhost    Database: module_db
 -- ------------------------------------------------------
--- Server version	8.0.29
+-- Server version	8.0.36
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
@@ -30,7 +28,7 @@ CREATE TABLE `module` (
   `description` varchar(255) DEFAULT NULL,
   `name` varchar(255) NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=10 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=16 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -39,7 +37,7 @@ CREATE TABLE `module` (
 
 LOCK TABLES `module` WRITE;
 /*!40000 ALTER TABLE `module` DISABLE KEYS */;
-INSERT INTO `module` VALUES (1,1,'A CI/CD course teaches the automation of software development workflows using Continuous Integration (CI) to merge and test code regularly, and Continuous Deployment (CD) to deliver updates efficiently and reliably.','Continuous Integration (CI) and Continuous Deployment (CD)'),(2,1,'An embedded systems course covers designing and programming specialized hardware-software systems, focusing on microcontrollers, sensors, real-time operations, and low-level programming.','Embedded Systems'),(3,1,'A machine learning with Python course focuses on building predictive models using Python libraries, covering data preprocessing, algorithms, and tools like NumPy, pandas, and scikit-learn.\n\n\n\n\n\n\n','Machine Learning with Python'),(4,2,'A financial accounting course teaches recording, summarizing, and reporting financial transactions to prepare accurate financial statements.','Financial Accounting'),(5,2,'A cost accounting course focuses on analyzing and managing costs to improve efficiency and support decision-making in business operations.','Cost Accounting'),(6,2,'A taxation course covers tax laws, compliance, and planning to prepare and manage individual and business tax obligations.','Taxation'),(7,3,'A course that explores the chemical processes and molecular structures in living organisms.','Biochemistry'),(8,3,'A course that examines the effects, mechanisms, and uses of drugs in biological systems.','Pharmacology'),(9,3,'A course focused on the structure, function, and manipulation of genes and cells.','Molecular Biology');
+INSERT INTO `module` VALUES (1,1,'A CI/CD module teaches the automation of software development workflows using Continuous Integration (CI) to merge and test code regularly, and Continuous Deployment (CD) to deliver updates efficiently and reliably.','Continuous Integration (CI) and Continuous Deployment (CD)'),(2,1,'An embedded systems module covers designing and programming specialized hardware-software systems, focusing on microcontrollers, sensors, real-time operations, and low-level programming.','Embedded Systems'),(3,1,'A machine learning with Python module focuses on building predictive models using Python libraries, covering data preprocessing, algorithms, and tools like NumPy, pandas, and scikit-learn.\r \r \r \r \r \r \r ','Machine Learning with Python'),(4,2,'A financial accounting module teaches recording, summarizing, and reporting financial transactions to prepare accurate financial statements.','Financial Accounting'),(5,2,'A cost accounting module focuses on analyzing and managing costs to improve efficiency and support decision-making in business operations.','Cost Accounting'),(6,2,'A taxation module covers tax laws, compliance, and planning to prepare and manage individual and business tax obligations.','Taxation'),(7,3,'A module that explores the chemical processes and molecular structures in living organisms.','Biochemistry'),(8,3,'A module that examines the effects, mechanisms, and uses of drugs in biological systems.','Pharmacology'),(9,3,'A module focused on the structure, function, and manipulation of genes and cells.','Molecular Biology'),(10,4,'Focuses on the principles and framework of a country\'s constitution, exploring the distribution of government powers, individual rights, and the interpretation of laws within a constitutional context.','Constitutional Law'),(11,4,'Examines the definition of criminal offenses, the legal processes involved in prosecution and defense, and the system of punishments designed to maintain order and justice in society.','Criminal Law'),(12,4,'Covers the creation, interpretation, and enforcement of legal agreements between parties, including topics like offer, acceptance, breach, and remedies.','Contracts'),(13,5,'Focuses on designing and analyzing buildings, bridges, and other structures to ensure they are safe, stable, and can withstand various forces.','Structural Engineering'),(14,5,'Involves planning, designing, and maintaining transportation systems, including roads, highways, and public transit, to ensure safe and efficient movement of people and goods.','Transportation Engineering'),(15,5,'Addresses the design and implementation of systems to protect the environment, including water treatment, waste management, and pollution control.','Environmental Engineering');
 /*!40000 ALTER TABLE `module` ENABLE KEYS */;
 UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
@@ -52,4 +50,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2024-12-19 12:00:23
+-- Dump completed on 2024-12-22 17:48:15
