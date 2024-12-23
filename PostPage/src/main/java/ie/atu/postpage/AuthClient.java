2127aa5adf6fd8094be97070778fa1bcdbeda37f
G00419108@atu.ie <G00419108@atu.ie>
2024-12-23 12:54:47 +0000

    Added add post (needs to be username not name)

--- PostPage/src/main/java/ie/atu/postpage/AuthClient.java
+++ PostPage/src/main/java/ie/atu/postpage/AuthClient.java
@@ -1,7 +1,9 @@
 package ie.atu.postpage;
 
 import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 
 @FeignClient(name="auth-service", url = "http://localhost:8083/auth")
 public interface AuthClient {
@@ -13,4 +15,7 @@ public interface AuthClient {
     String getSignedEmail();
     @GetMapping("/userType")
     boolean isModerator();
+
+    @GetMapping("/user/{username}/id")
+    ResponseEntity<Long> getUserIdByUsername(@PathVariable("username") String username);
 }
